382b2d58c4b7113287999423bd265c2a90e4a86a
mhansinger <maximilian.hansinger@gmail.com>
2017-04-29 13:28:42 +0200

    Stream Beispiel added

---
+++ Stream/Beispiel_stream.py
@@ -0,0 +1,12 @@
+# kleines Beispiel zum Streamen
+
+import stream_series
+
+URL = URL = 'http://zeiselmair.de/h4ckamuenster/results.txt'
+
+ETH_stream = stream_series(URL,20)
+
+ETH_stream.start()
+
+# zum Pausieren: 
+#ETH_stream.pause()
