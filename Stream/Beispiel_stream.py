d071a03d5200119df21fb47c39a68e8986033f1c
mhansinger <maximilian.hansinger@gmail.com>
2017-04-29 13:35:15 +0200

    Update Beispiel_stream.py

--- Stream/Beispiel_stream.py
+++ Stream/Beispiel_stream.py
@@ -2,7 +2,7 @@
 
 import stream_series
 
-URL = URL = 'http://zeiselmair.de/h4ckamuenster/results.txt'
+URL = 'http://zeiselmair.de/h4ckamuenster/results.txt'
 
 ETH_stream = stream_series(URL,20)
 
